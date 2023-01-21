1bb9fd9157420a708846da3cde5978f6b375b9d2
Peter Hansen <peter@engcorp.com>
2023-01-20 23:01:35 -0500

    experimental autonomous sequence with a home-brewed crappy generator-based sequencer

--- robot.py
+++ robot.py
@@ -48,9 +48,60 @@ class MyRobot(wpilib.TimedRobot):
         self.driveStick = self.buildDriveStick()
 
 
+    def disabledInit(self):
+        self.myRobot.arcadeDrive(0, 0)
+
+
+    def autonomousInit(self):
+        """Executed at the start of autonomous mode"""
+        self.myRobot.setSafetyEnabled(True)
+        self.phase = self.run_phases()
+
+    # crude substitute for Command stuff
+    PHASES = [
+        (1.0, 'initial'),
+        (4.0, 'curve_out'),
+        (1.1, 'pivot_left'),
+        (2.5, 'straight'),
+        (5.0, 'back_right'),
+        ]
+
+    def _phase_initial(self):       self.myRobot.arcadeDrive(0, 0)
+    def _phase_curve_out(self):     self.myRobot.arcadeDrive(-0.1, -0.7)
+    def _phase_pivot_left(self):    self.myRobot.arcadeDrive(-0.3, -0.5)
+    def _phase_straight(self):      self.myRobot.arcadeDrive(0, -0.80)
+    def _phase_back_right(self):    self.myRobot.arcadeDrive(-0.2, 0.6)
+
+    def run_phases(self):
+        self._phase = 0
+        timer = wpilib.Timer()
+        timer.start()
+
+        for (duration, name) in self.PHASES:
+            self.logger.info('phase: %s (%.1fs)', name, duration)
+
+            handler = getattr(self, f'_phase_{name}')
+
+            timer.reset()
+            while timer.get() < duration:
+                # self.logger.info('phase: %s @%.1fs', name, timer.get())
+                handler()
+                yield
+
+
+    def autonomousPeriodic(self):
+        """Autonomous sequence"""
+        try:
+            next(self.phase)
+        except StopIteration:
+            self.myRobot.arcadeDrive(0, 0)
+            # breakpoint()
+
+
     def teleopInit(self):
         """Executed at the start of teleop mode"""
         self.myRobot.setSafetyEnabled(True)
+        # self.logger.info('', self.getBatteryVoltage())
 
 
     def teleopPeriodic(self):
